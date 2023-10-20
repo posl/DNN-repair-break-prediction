eb82bc64084a2b57e07adc17b2e753a555a721c6
y629 <yuutaishimoto@gmail.com>
2023-10-20 07:02:21 +0000

    [refactoring] src to draw rep. bre. violin

--- src/preprocess_repair_break_dataset.py
+++ src/preprocess_repair_break_dataset.py
@@ -1,6 +1,5 @@
 import os, sys
 from collections import defaultdict
-from itertools import product
 import pandas as pd
 import numpy as np
 from lib.util import json2dict
@@ -32,19 +31,10 @@ def outlier_iqr(df, obj_col):
     return tmp_df
 
 
-# TODO: これらの定数を外部化
+# NOTE: 定数（外部化しなくてもいっかな）
 # =====================================================
-# 対象とするrepair手法のリスト
-methods = ["care", "apricot"]
 # 対象とするdivision (train, repair, testのいずれか)
 divisions = ["train", "repair", "test"]
-
-# データセット名などのセッティングの辞書
-conf_dic = {
-    "credit": {"sens_name": "gender", "num_folds": 5},
-    "census": {"sens_name": "gender", "num_folds": 10},
-    "bank": {"sens_name": "age", "num_folds": 10},
-}
 # 説明変数の列名のリスト
 exp_metrics = ["pcs", "lps", "loss", "entropy"]
 # testの割合
@@ -59,106 +49,94 @@ sid = 1
 if __name__ == "__main__":
     # このプログラムのファイル名を取得
     file_name = os.path.splitext(sys.argv[0])[0]
+    # repair methodとdatasetはstdinから受け取る
+    method = sys.argv[1]
+    dataset = sys.argv[2]
 
-    df_corr_ld = defaultdict(pd.DataFrame)
-    df_fair_ld = defaultdict(pd.DataFrame)
-    # dic = {"correctness": df_corr_ld, "fairness": df_fair_ld}
-    dic = {"correctness": df_corr_ld}
+    # 実験のディレクトリと実験名を取得
+    exp_dir = f"/src/experiments/{method}"
+    # ログファイルの生成
+    logger = set_exp_logging(exp_dir, f'{dataset}-{file_name.replace("_", "-")}')
+    # 最終的な出力物の格納ディレクトリの生成
+    save_dir = os.path.join(exp_dir, "repair_break_dataset", "preprocessed_data")
+    os.makedirs(save_dir, exist_ok=True)
 
-    for _, df_ld in dic.items():
-        for dataset, method in product(conf_dic.keys(), methods):
-            # 実験のディレクトリと実験名を取得
-            exp_dir = f"/src/experiments/{method}"
-            # sensitive feature名
-            sens_name = conf_dic[dataset]["sens_name"]
-            # ログファイルの生成
-            logger = set_exp_logging(exp_dir, f'{dataset}-{file_name.replace("_", "-")}')
-            # 最終的な出力物の格納ディレクトリの生成
-            save_dir = os.path.join(exp_dir, "repair_break_dataset", "preprocessed_data")
-            os.makedirs(save_dir, exist_ok=True)
+    for rb in ["repair", "break"]:
+        logger.info(f"method={method}, dataset={dataset}, rb={rb}")
+        # repair/break datasetのファイル名
+        if method == "care":
+            rb_ds_filename = f"{dataset}-fairness-setting{sid}-{rb}.csv"
+        elif method == "apricot" or method == "arachne":
+            rb_ds_filename = f"{dataset}-training-setting{sid}-{rb}.csv"
+        # repair/break datasetのcsvをロード
+        rb_ds_path = os.path.join(exp_dir, "repair_break_dataset", "raw_data", rb_ds_filename)
+        df = pd.read_csv(rb_ds_path)
+        logger.info(f"original df shape: {df.shape}")
 
-            for rb in ["repair", "break"]:
-                logger.info(f"method={method}, dataset={dataset}, rb={rb}")
-                # repair/break datasetのcsvをロード
-                rb_ds_filename = (
-                    f"{dataset}-fairness-{sens_name}-setting{sid}-{rb}.csv"
-                    if method == "care"
-                    else f"{dataset}-training-setting{sid}-{rb}.csv"
-                )
-                rb_ds_path = os.path.join(exp_dir, "repair_break_dataset", "raw_data", rb_ds_filename)
-                df = pd.read_csv(rb_ds_path)
-                logger.info(f"original df shape: {df.shape}")
+        # 目的変数と説明変数を取得
+        obj_col = "repaired" if rb == "repair" else "broken"
+        feats = [feat for feat in list(df.columns) if feat != obj_col]
 
-                # 目的変数と説明変数を取得
-                obj_col = "repaired" if rb == "repair" else "broken"
-                feats = [feat for feat in list(df.columns) if feat != obj_col]
+        # 異常値の除去を行う (breakだけ)
+        # if rb == "break":
+        #     logger.info("Elimination of anomaly values...")
+        #     df = outlier_iqr(df, obj_col)
+        #     logger.info(f"After elimination df shape: {df.shape}")
 
-                # 異常値の除去を行う (breakだけ)
-                # if rb == "break":
-                #     logger.info("Elimination of anomaly values...")
-                #     df = outlier_iqr(df, obj_col)
-                #     logger.info(f"After elimination df shape: {df.shape}")
+        # trainval / test に分割
+        logger.info("Dividing into trainval / test...")
+        df_train, df_test = train_test_split(df, test_size=test_ratio, random_state=random_state)
+        logger.info(f"df_train.shape: {df_train.shape}, df_test.shape: {df_test.shape}")
 
-                # trainval / test に分割
-                logger.info("Dividing into trainval / test...")
-                df_train, df_test = train_test_split(df, test_size=test_ratio, random_state=random_state)
-                logger.info(f"df_train.shape: {df_train.shape}, df_test.shape: {df_test.shape}")
+        # trainvalの方に標準化を行う
+        logger.info("Standardization...")
+        std = StandardScaler()
+        std.fit(df_train[feats])
+        df_train[feats] = std.transform(df_train[feats])
+        # trainvalと同様の変換をtestに適用
+        df_test[feats] = std.transform(df_test[feats])
 
-                # trainvalの方に標準化を行う
-                logger.info("Standardization...")
-                std = StandardScaler()
-                std.fit(df_train[feats])
-                df_train[feats] = std.transform(df_train[feats])
-                # trainvalと同様の変換をtestに適用
-                df_test[feats] = std.transform(df_test[feats])
+        # trainvalの方にyeo-johnson変換（と，その後に標準化）を行う
+        logger.info("Yeo-Johnson transformation and Standardization...")
+        pt = PowerTransformer(method="yeo-johnson", standardize=True)
+        pt.fit(df_train[feats])
+        df_train[feats] = pt.transform(df_train[feats])
+        # trainvalと同様の変換をtestに適用
+        df_test[feats] = pt.transform(df_test[feats])
 
-                # trainvalの方にyeo-johnson変換（と，その後に標準化）を行う
-                logger.info("Yeo-Johnson transformation and Standardization...")
-                pt = PowerTransformer(method="yeo-johnson", standardize=True)
-                pt.fit(df_train[feats])
-                df_train[feats] = pt.transform(df_train[feats])
-                # trainvalと同様の変換をtestに適用
-                df_test[feats] = pt.transform(df_test[feats])
+        # trainvalの方をcsvで保存
+        if method == "care":
+            rb_ds_trainval_filename = f"{dataset}-fairness-setting{sid}-{rb}-trainval.csv"
+        elif method == "apricot" or method == "arachne":
+            rb_ds_trainval_filename = f"{dataset}-training-setting{sid}-{rb}-trainval.csv"
+        rb_ds_trainval_path = os.path.join(
+            exp_dir, "repair_break_dataset", "preprocessed_data", rb_ds_trainval_filename
+        )
+        df_train.to_csv(rb_ds_trainval_path, index=False)
 
-                # 作成したdfをコピーして別の変数に保存
-                df_ld[rb] = df_train.copy()
+        # testの方をcsvで保存
+        if method == "care":
+            rb_ds_test_filename = f"{dataset}-fairness-setting{sid}-{rb}-test.csv"
+        elif method == "apricot" or method == "arachne":
+            rb_ds_test_filename = f"{dataset}-training-setting{sid}-{rb}-test.csv"
+        rb_ds_test_path = os.path.join(exp_dir, "repair_break_dataset", "preprocessed_data", rb_ds_test_filename)
+        df_test.to_csv(rb_ds_test_path, index=False)
 
-                # trainvalの方をcsvで保存
-                rb_ds_trainval_filename = (
-                    f"{dataset}-fairness-{sens_name}-setting{sid}-{rb}-trainval.csv"
-                    if method == "care"
-                    else f"{dataset}-training-setting{sid}-{rb}-trainval.csv"
-                )
-                rb_ds_trainval_path = os.path.join(
-                    exp_dir, "repair_break_dataset", "preprocessed_data", rb_ds_trainval_filename
-                )
-                df_train.to_csv(rb_ds_trainval_path, index=False)
+        # 保存はしないけど, 描画用でtrainval+testのdfも作っとく
+        df_all = pd.concat([df_train, df_test])
+        logger.info(f"df_all.shape: {df_all.shape}")
 
-                # testの方をcsvで保存
-                rb_ds_test_filename = (
-                    f"{dataset}-fairness-{sens_name}-setting{sid}-{rb}-test.csv"
-                    if method == "care"
-                    else f"{dataset}-training-setting{sid}-{rb}-test.csv"
-                )
-                rb_ds_test_path = os.path.join(
-                    exp_dir, "repair_break_dataset", "preprocessed_data", rb_ds_test_filename
-                )
-                df_test.to_csv(rb_ds_test_path, index=False)
-
-            # violinplotにまとめる
-            for rb in ["repair", "break"]:
-                print(f"Making violinplot of trainval set of method={method}, dataset={dataset}, rb={rb}...")
-                obj_col = "repaired" if rb == "repair" else "broken"
-                ltrue = df_ld[rb][obj_col].sum()
-                print(
-                    f"df_corr_ld[rb].shape: {df_corr_ld[rb].shape}, #{obj_col}: {ltrue}, #non-{obj_col}: {len(df_ld[rb]) - ltrue}"
-                )
-                fig, axes = plt.subplots(nrows=1, ncols=4, sharex=False, figsize=(12, 3))
-                plt.subplots_adjust(wspace=0.2, hspace=0.2)
-                for task_name, df_ld in dic.items():
-                    for i, feat in enumerate(feats):
-                        df = df_ld[rb]
-                        sns.violinplot(data=df, x=obj_col, y=feat, palette="Greys", ax=axes[i], split=True)
-                fig.tight_layout()
-                fig_path = os.path.join(save_dir, f"{dataset}-trainval-{rb}.pdf")
-                plt.savefig(fig_path, bbox_inches="tight")
+        # violinplotにまとめる
+        for mode, df in zip(["trainval", "test", "all"], [df_train, df_test, df_all]):
+            logger.info(f"Making violinplot of {mode} set of method={method}, dataset={dataset}, rb={rb}...")
+            obj_col = "repaired" if rb == "repair" else "broken"
+            ltrue = df[obj_col].sum()
+            logger.info(f"df_{mode}[rb].shape: {df.shape}, #{obj_col}: {ltrue}, #non-{obj_col}: {len(df) - ltrue}")
+            fig, axes = plt.subplots(nrows=1, ncols=4, sharex=False, figsize=(12, 3))
+            plt.subplots_adjust(wspace=0.2, hspace=0.2)
+            for i, feat in enumerate(feats):
+                sns.violinplot(data=df, x=obj_col, y=feat, palette="Greys", ax=axes[i], split=True)
+            fig.tight_layout()
+            fig_path = os.path.join(save_dir, f"{dataset}-{mode}-{rb}.pdf")
+            plt.savefig(fig_path, bbox_inches="tight")
+            logger.info(f"saved to {fig_path}.")
