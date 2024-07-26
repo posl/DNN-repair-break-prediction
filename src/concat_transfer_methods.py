b5df383e4dc48d89a6e21688de7d4d924fa1c804
y629 <yuutaishimoto@gmail.com>
2024-07-26 18:09:15 +0900

    add compatibility calculation

--- src/concat_transfer_methods.py
+++ src/concat_transfer_methods.py
@@ -15,12 +15,14 @@ if __name__ == "__main__":
     datasets = ["credit", "census", "bank", "fm", "c10", "gtsrb", "imdb", "rtmr"]
     # 保存先のディレクトリ
     transferability_dir = os.path.join("/src/experiments/", "method-transferability")
+    compatibility_dir = os.path.join("/src/experiments/", "method-compatibility")
     
     # 各組み合わせについて実行
     for rb in ["repair", "break"]:
-        trans_df_list = []
-        for dataset in datasets:
-            trans_df_list.append(pd.read_csv(os.path.join(transferability_dir, f"{dataset}-{rb}.csv")))
-        # trans_df_listを縦に結合
-        trans_df = pd.concat(trans_df_list)
-        trans_df.to_csv(os.path.join(transferability_dir, f"all-{rb}.csv"), index=False)
+        for dir in [transferability_dir, compatibility_dir]:
+            df_list = []
+            for dataset in datasets:
+                df_list.append(pd.read_csv(os.path.join(dir, f"{dataset}-{rb}.csv")))
+            # trans_df_listを縦に結合
+            trans_df = pd.concat(df_list)
+            trans_df.to_csv(os.path.join(dir, f"all-{rb}.csv"), index=False)
--- src/transfer_methods.py
+++ src/transfer_methods.py
@@ -41,6 +41,14 @@ def get_test_df(data_dir, method, ds, rb):
     test_ds_dir = os.path.join(data_dir, test_csv_name)
     return pd.read_csv(test_ds_dir)
 
+def get_compatibility(clf1, clf2, X):
+    """
+    clf1, clf2の予測結果の互換性を計測する
+    """
+    y1 = clf1.predict(X)
+    y2 = clf2.predict(X)
+    return np.mean(y1 == y2)
+
 if __name__ == "__main__":
     # このプログラムのファイル名を取得
     file_name = os.path.splitext(sys.argv[0])[0]
@@ -48,9 +56,12 @@ if __name__ == "__main__":
     dataset = sys.argv[1]
     # 保存先のディレクトリ
     transferability_dir = os.path.join("/src/experiments/", "method-transferability")
+    compatibility_dir = os.path.join("/src/experiments/", "method-compatibility")
     # ディレクトリがなければ作成
     if not os.path.exists(transferability_dir):
         os.makedirs(transferability_dir, exist_ok=True)
+    if not os.path.exists(compatibility_dir):
+        os.makedirs(compatibility_dir, exist_ok=True)
 
     # 各組み合わせについて実行
     for rb in ["repair", "break"]:
@@ -58,32 +69,40 @@ if __name__ == "__main__":
         obj_col = "repaired" if rb == "repair" else "broken"
         # transferabilityの結果を保存するdf
         transferability_df = pd.DataFrame(columns=[methods], index=[methods], data=[])
+        compatibility_df = pd.DataFrame(columns=[methods], index=[methods], data=[])
         for mt_src, mt_tar in permutations(methods, 2):
             print(f"mt_src={mt_src}, mt_tar={mt_tar}, rb={rb}")
             # ディレクトリの指定
-            model_save_dir = os.path.join(f"/src/experiments/{mt_src}", "repair_break_model")
-            data_save_dir = os.path.join(f"/src/experiments/{mt_tar}", "repair_break_dataset", "preprocessed_data")
+            src_model_save_dir = os.path.join(f"/src/experiments/{mt_src}", "repair_break_model") # srcの修正手法のためのモデル
+            tar_model_save_dir = os.path.join(f"/src/experiments/{mt_tar}", "repair_break_model") # targetの修正手法のためのモデル
+            data_save_dir = os.path.join(f"/src/experiments/{mt_tar}", "repair_break_dataset", "preprocessed_data") # tgtの修正手法のためのモデル
             # datasetに対するmt_srcの予測モデルを読み込み
-            model_dic = get_models(model_save_dir, dataset, rb)
+            src_model_dic = get_models(src_model_save_dir, dataset, rb)
+            tar_model_dic = get_models(tar_model_save_dir, dataset, rb)
             # datasetに対するmt_tarのデータを読み込み
             df_test = get_test_df(data_save_dir, mt_tar, dataset, rb)
             X_test, y_test = df_test[exp_metrics], df_test[obj_col]
             # ds_srcのモデルでds_tarのテストデータを予測して結果をarrayにまとめる
-            test_res_arr = []
+            src_test_res_arr = []
+            tar_test_res_arr = []
             for model_name in ["lr", "rf", "lgb"]:
-                test_res_arr.append(print_perf(model_dic[model_name], X_test, y_test, output_log=False))
-            test_res_arr = np.asarray(test_res_arr)
+                src_test_res_arr.append(print_perf(src_model_dic[model_name], X_test, y_test, output_log=False))
+                tar_test_res_arr.append(get_compatibility(src_model_dic[model_name], tar_model_dic[model_name], X_test))
+            src_test_res_arr = np.asarray(src_test_res_arr)
+            tar_test_res_arr = np.asarray(tar_test_res_arr)
             
             # 元々のデータセットでの精度と比較する
-            org_res_path = os.path.join(model_save_dir, f"{dataset}-{rb}-test.csv")
+            org_res_path = os.path.join(f"/src/experiments/{mt_tar}", "repair_break_model", f"{dataset}-{rb}-test.csv")
             org_res_df = pd.read_csv(org_res_path)
             org_res_arr = org_res_df.values
 
             # test_res_arrをorg_res_arrで割ることで，originalとの差分を確認
             # test_res_arr, org_res_arrに0があればnanにする
-            test_res_arr[test_res_arr == 0] = np.nan
+            src_test_res_arr[src_test_res_arr == 0] = np.nan
             org_res_arr[org_res_arr == 0] = np.nan
             # nanを除いて平均してdfに格納
-            transferability_df.loc[mt_src, mt_tar] = np.nanmean(test_res_arr / org_res_arr)
+            transferability_df.loc[mt_src, mt_tar] = np.nanmean(src_test_res_arr / org_res_arr)
+            compatibility_df.loc[mt_src, mt_tar] = np.mean(tar_test_res_arr)
         # 結果を保存
         transferability_df.to_csv(os.path.join(transferability_dir, f"{dataset}-{rb}.csv"))
+        compatibility_df.to_csv(os.path.join(compatibility_dir, f"{dataset}-{rb}.csv"))
